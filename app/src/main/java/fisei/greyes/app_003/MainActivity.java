d5fc29fc22c86d9423d1d9fc2eee3fa09371c012
reguido99 <reguido99@gmail.com>
2021-07-07 20:03:31 -0500

    todos los estados de una actividad

--- app/src/main/java/fisei/greyes/app_003/MainActivity.java
+++ app/src/main/java/fisei/greyes/app_003/MainActivity.java
@@ -15,5 +15,39 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"Evento onCreate",Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"Evento: onStart",Toast.LENGTH_SHORT).show();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,"Evento: onStop",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"Evento: onDestroy",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"Evento: onResume",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,"Evento: onPause",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,"Evento: onRestart",Toast.LENGTH_SHORT).show();
+    }
 }
