d23c441da42e6ed08852fb3d394db1f94300634c
reguido99 <reguido99@gmail.com>
2021-07-07 19:39:49 -0500

    nuevo cambio, evento onCreate

--- app/src/main/java/fisei/greyes/app_003/MainActivity.java
+++ app/src/main/java/fisei/greyes/app_003/MainActivity.java
@@ -3,6 +3,7 @@ package fisei.greyes.app_003;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"Evento onCreate",Toast.LENGTH_SHORT).show();
     }
+
+
 }
